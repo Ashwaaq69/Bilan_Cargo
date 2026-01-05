b6fcd0ea85943ead6befb6bcbe910c0b4b301c84
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2026-01-05 14:39:20 +0300

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300">
+    <footer className="bg-blue-600 text-gray-300">
       <div className="max-w-7xl mx-auto px-6 py-8 text-center">
         <p>
           © {new Date().getFullYear()} BILAN CARGO. All rights reserved.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar() {
         </h1>
 
         <ul className="hidden md:flex space-x-6 font-medium">
-          <li className="hover:text-blue-600 cursor-pointer">Home</li>
+          <li><a href="#home" className="hover:text-blue-600 cursor-pointer">Home </a></li>
           <li><a href="#about" className="hover:text-blue-600">About</a></li>
-          <li className="hover:text-blue-600 cursor-pointer">Services</li>
+          <li><a href="#services" className="hover:text-blue-600 cursor-pointer">Services</a></li>
           <li><a href="#contact" className="hover:text-blue-600">Contact</a></li>
 
         </ul>
